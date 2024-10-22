632209276e5e1310a6d2348e2f214ea92f0af5c3
Divyank Shah <dv491067@dal.ca>
2024-10-22 00:28:42 -0300

    feat: user entity created.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './user/user.module';
+import { IncomeModule } from './income/income.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     UsersModule,
+    IncomeModule,
   ],
   controllers: [],
   providers: [],
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -7,7 +7,7 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { UsersService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
