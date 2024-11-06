b0901e48bd58d7ff87b1628364a8715b5ec5220a
Divyank Shah <dv491067@dal.ca>
2024-11-06 15:17:23 -0400

    feat: method to get category from database based on name added.

--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { EntityManager, Repository } from 'typeorm';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+    private readonly entityManager: EntityManager,
+  ) {}
+
+  async findByName(name: string){
+    return this.categoryRepository.findOneBy({name})
+  }
+
+}
--- backend/src/expense/dto/create-expense.dto.ts
+++ backend/src/expense/dto/create-expense.dto.ts
@@ -1,11 +1,8 @@
 import { Category } from 'src/category/entities/category.entity';
-import { User } from 'src/user/entities/user.entity';
 
 export class CreateExpenseDto {
   title: string;
   description: string;
   amount: number;
   date: Date;
-  user: User;
-  category: Category;
 }
