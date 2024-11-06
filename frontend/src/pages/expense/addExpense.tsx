da830048a14eb99f7b0af9ca0044fb09bb35d357
Divyank Shah <dv491067@dal.ca>
2024-11-06 17:08:26 -0400

    feat: add expense form completed.

---
+++ frontend/src/pages/expense/addExpense.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { HeaderText } from "../../components/text/HeaderText";
+import { TextInput } from "../../components/input/TextInput";
+import { PrimaryButton } from "../../components/button/PrimaryButton";
+import { NumberInput } from "../../components/input/NumberInput";
+import DatePickerInput from "../../components/input/DatePickerInput";
+import SelectInput from "../../components/input/SelectInput";
+
+const AddExpense = () => {
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [amount, setAmount] = useState<number>(0);
+  const [date, setDate] = useState<string>("");
+  const [category, setCategory] = useState<string>("");
+
+  const callAddExpense = () => {
+    console.log("title: ", title);
+    console.log("description: ", description);
+    console.log("amount: ", amount);
+    console.log("date: ", date);
+    console.log("category: ", category);
+  };
+
+  return (
+    <>
+      <div className="my-5 text-center">
+        <HeaderText label="Add Expense" />
+        <div className="w-1/3 text-center p-2 mx-auto my-2">
+          <TextInput
+            label="Title"
+            labelPosition="top"
+            placeholderText="Title"
+            value={title}
+            onChange={(value) => setTitle(value)}
+            type="string"
+          />
+          <TextInput
+            label="Description"
+            labelPosition="top"
+            placeholderText="Description"
+            value={description}
+            onChange={(value) => setDescription(value)}
+            type="string"
+          />
+          <NumberInput
+            label="Amount"
+            labelPosition="top"
+            placeholderText="Amount"
+            value={amount}
+            onChange={(value) => setAmount(value)}
+            type="number"
+          />
+
+          <DatePickerInput
+            label="Date"
+            labelPosition="top"
+            value={date}
+            onChange={(value) => setDate(value)}
+          />
+
+          <SelectInput
+            label="Category"
+            labelPosition="top"
+            value={category}
+            onChange={(value) => setCategory(value)}
+            placeholderText="Select Category"
+          />
+
+          <PrimaryButton
+            buttonText="Add Income"
+            onClick={async () => {
+              await callAddExpense();
+            }}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AddExpense;
