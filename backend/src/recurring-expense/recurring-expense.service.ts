1c954132a9408d92cb0a3aa2f5206802a4b12f21
Divyank Shah <dv491067@dal.ca>
2024-12-06 22:16:58 -0400

    bug fix

--- backend/src/recurring-expense/recurring-expense.service.ts
+++ backend/src/recurring-expense/recurring-expense.service.ts
@@ -41,43 +41,6 @@ export class RecurringExpenseService {
     return nextDate;
   };
 
-  // Helper function to calculate the next recurrence date based on frequency
-  // calculateNextDate = (currentDate: Date, frequency: string) => {
-  //   const nextDate = new Date(currentDate);
-
-  //   switch (frequency) {
-  //     case 'daily':
-  //       nextDate.setUTCDate(nextDate.getUTCDate() + 1); // Add 1 day for daily recurrence
-  //       break;
-  //     case 'weekly':
-  //       nextDate.setUTCDate(nextDate.getUTCDate() + 7); // Add 7 days for weekly recurrence
-  //       break;
-  //     case 'bi-weekly':
-  //       nextDate.setUTCDate(nextDate.getUTCDate() + 14); // Add 14 days for bi-weekly recurrence
-  //       break;
-  //     case 'tri-weekly':
-  //       nextDate.setUTCDate(nextDate.getUTCDate() + 21); // Add 21 days for tri-weekly recurrence
-  //       break;
-  //     case 'monthly':
-  //       // Adjust for monthly recurrence, ensuring the next date is the same day of the month
-  //       const currentMonth = nextDate.getUTCMonth();
-  //       nextDate.setUTCMonth(currentMonth + 1);
-
-  //       // If the next month has fewer days than the current day, adjust the date to the last day of the next month
-  //       if (nextDate.getUTCMonth() !== (currentMonth + 1) % 12) {
-  //         nextDate.setUTCDate(0); // This sets the date to the last day of the previous month (next month)
-  //       }
-
-  //       // Set time to midnight (00:00:00) to avoid issues with time zone differences
-  //       nextDate.setUTCHours(0, 0, 0, 0);
-  //       break;
-  //     default:
-  //       throw new Error('Invalid frequency');
-  //   }
-
-  //   return nextDate;
-  // };
-
   constructor(
     @InjectRepository(RecurringExpense)
     private readonly recurringExpenseRepository: Repository<RecurringExpense>,
